9f09924a1ba3274923b46f1b73cbea39f6db4408
AIME-LOIC <kingaime132@gmail.com>
2026-05-18 20:27:13 +0200

    issue #30 ADMIN_API_KEY not validated at startup - empty value causes 500 on every protected request solved by AIME-LOIC

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import app from "./app";
 import { env } from "./config/env";
 
+if (!env.adminApiKey) {
+  console.warn(
+    "WARNING: ADMIN_API_KEY is not set. All admin endpoints will return 500.",
+  );
+}
+
 app.listen(env.port, () => {
   console.log(`Server running on http://localhost:${env.port}`);
 });
